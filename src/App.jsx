a55612d8a6c4c9c477be6a48777dcbce111ef50a
Sean <63425198+seanjulagay@users.noreply.github.com>
2023-11-23 23:35:31 +0800

    fin

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Main from "./components/Main";
 import Navbar from "./components/Navbar";
+import "./scss/globals.scss";
 
 export default function App() {
   return (
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,22 +1,31 @@
 import "./Main.scss";
+import reactLogoHalf from "../images/react-logo-half.png";
 
 export default function Main() {
   return (
-    <div className="main">
-      <div className="main-container">
-        <h1 className="main-title">Fun facts about React</h1>
-        <ul className="main-list">
-          <li className="main-list-item">Was first released in 2013</li>
-          <li className="main-list-item">
-            Was originally created by Jordan Walke
-          </li>
-          <li className="main-list-item">Has well over 100K stars on GitHub</li>
-          <li className="main-list-item">Is maintained by Facebook</li>
-          <li className="main-list-item">
-            Powers thousands of enterprise apps, including mobile apps
-          </li>
-        </ul>
+    <>
+      <div className="main">
+        <div className="main-container">
+          <h1 className="main-title">Fun facts about React</h1>
+          <ul className="main-list">
+            <li className="main-list-item">Was first released in 2013</li>
+            <li className="main-list-item">
+              Was originally created by Jordan Walke
+            </li>
+            <li className="main-list-item">
+              Has well over 100K stars on GitHub
+            </li>
+            <li className="main-list-item">Is maintained by Facebook</li>
+            <li className="main-list-item">
+              Powers thousands of enterprise apps, including mobile apps
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
+      <div className="react-logo-container">
+        <div className="react-logo-space" />
+        <img src={reactLogoHalf} alt="" className="react-logo-img" />
+      </div>
+    </>
   );
 }
